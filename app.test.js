0503b79b7a4271dc01965947287517b1870d61e6
Xiaoyi423 <96610420+Xiaoyi423@users.noreply.github.com>
2022-12-11 01:51:53 -0500

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -5,7 +5,6 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-//Register
 const person = {
   username: 'lxy',
   password: 'Abc123',
@@ -19,6 +18,13 @@ const person3 = {
   password: 'Abc1234',
 };
 
+const person4 = {
+  username: 'Ina',
+  password: 'Abc1234',
+};
+
+let token;
+//Register
 describe('/POST register', () => {
   it('it should register successfully when the credentials are valid', (done) => {
     chai
@@ -55,6 +61,7 @@ describe('/POST login', () => {
         res.should.have.status(200);
         res.body.should.be.a('object');
         res.body.should.have.property('msg').eql('Log in Success');
+        token = res.body.token;
         done();
       });
   });
@@ -79,6 +86,106 @@ describe('/POST login', () => {
         res.should.have.status(401);
         res.body.should.be.a('object');
         res.body.should.have.property('msg').eql('Incorrect Password/Username');
+        done();
+      });
+  });
+});
+
+const wrongID = '239579c7087237149ef01a34';
+const post = {
+  content: 'trial',
+};
+
+let post_id;
+describe('/POST createPost', () => {
+  it('it should create a post', (done) => {
+    chai
+      .request(server)
+      .post('/createPost')
+      .auth(token, { type: 'bearer' })
+      .send(post)
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        post_id = res.body._id;
+        done();
+      });
+  });
+});
+
+describe('/GET postId', () => {
+  it('it should load a post', (done) => {
+    chai
+      .request(server)
+      .get('/' + post_id)
+      .auth(token, { type: 'bearer' })
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('content').eql('trial');
+        done();
+      });
+  });
+  it('it should not lead a post since the id does not exist', (done) => {
+    chai
+      .request(server)
+      .get('/' + wrongID)
+      .auth(token, { type: 'bearer' })
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('Post does not exist');
+        done();
+      });
+  });
+});
+
+describe('/Put postId', () => {
+  it('it should update a post', (done) => {
+    const update = { newContent: 'updated' };
+    chai
+      .request(server)
+      .put('/' + post_id)
+      .auth(token, { type: 'bearer' })
+      .send(update)
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('content').eql('updated');
+        done();
+      });
+  });
+  it('it should not update a post since the id does not exist', (done) => {
+    const update = { newContent: 'updated' };
+    chai
+      .request(server)
+      .put('/' + wrongID)
+      .auth(token, { type: 'bearer' })
+      .send(update)
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('Post does not exist');
+        done();
+      });
+  });
+});
+describe('/Delete postId', () => {
+  it('it should delete a post', (done) => {
+    chai
+      .request(server)
+      .delete('/' + post_id)
+      .auth(token, { type: 'bearer' })
+      .send()
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('delete successful');
+        done();
+      });
+  });
+  it('it should not delete a post since the id does not exist', (done) => {
+    chai
+      .request(server)
+      .delete('/' + wrongID)
+      .auth(token, { type: 'bearer' })
+      .end((err, res) => {
+        res.body.should.be.a('object');
+        res.body.should.have.property('msg').eql('Post does not exist');
         done();
       });
   });
